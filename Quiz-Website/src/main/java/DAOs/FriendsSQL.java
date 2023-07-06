ff57e9b80273839bb1692fbdc8049fa26c97a2e8
David Dadiani <ddadi21@freeuni.edu.ge>
2023-07-06 19:59:15 +0400

    Added more decomposition

--- Quiz-Website/src/main/java/DAOs/FriendsSQL.java
+++ Quiz-Website/src/main/java/DAOs/FriendsSQL.java
@@ -58,26 +58,30 @@ public class FriendsSQL implements FriendsDao {
         }
     }
 
+    private PreparedStatement prepareStatement(String query, long val1, long val2){
+
+        try {
+            Connection connection = dataSource.getConnection();
+
+            PreparedStatement result = connection.prepareStatement(query);
+            result.setLong(1, val1);
+            result.setLong(2, val2);
+
+            return result;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     // Adds a friendship between two users
     @Override
     public int addFriendship(Long user_id1, Long user_id2) {
         if (checkIfFriends(user_id1, user_id2)) return FAILED_ADDED;
 
         try {
-            // Establish a database connection
-            Connection connection = dataSource.getConnection();
-
-            // Prepare the first SQL query to add the friendship
-            String query1 = "INSERT INTO friendships(first_user_id,second_user_id) VALUES(?,?)";
-            PreparedStatement preparedStatement1 = connection.prepareStatement(query1);
-            preparedStatement1.setLong(1, user_id1);
-            preparedStatement1.setLong(2, user_id2);
-
-            // Prepare the second SQL query to add the friendship in the opposite direction
-            String query2 = "INSERT INTO friendships(first_user_id,second_user_id) VALUES(?,?)";
-            PreparedStatement preparedStatement2 = connection.prepareStatement(query2);
-            preparedStatement2.setLong(1, user_id2);
-            preparedStatement2.setLong(2, user_id1);
+            PreparedStatement preparedStatement1 = prepareStatement("INSERT INTO friendships(first_user_id,second_user_id) VALUES(?,?)", user_id1, user_id2);
+            PreparedStatement preparedStatement2 = prepareStatement("INSERT INTO friendships(first_user_id,second_user_id) VALUES(?,?)", user_id2, user_id1);
 
             // Execute the queries and check if any rows were affected
             if(preparedStatement2.executeUpdate() != 0 && preparedStatement1.executeUpdate() != 0) return SUCCESS_ADDED;
@@ -91,17 +95,12 @@ public class FriendsSQL implements FriendsDao {
     @Override
     public int removeFriendship(Long user_id1, Long user_id2) {
         try {
-            // Establish a database connection
-            Connection connection = dataSource.getConnection();
 
-            // Prepare the SQL query to remove the friendship
-            String query = "delete from friendships where (first_user_id = ? and second_user_id = ?) or (first_user_id = ? and second_user_id = ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setLong(1, user_id1);
-            preparedStatement.setLong(2, user_id2);
-            preparedStatement.setLong(3, user_id2);
-            preparedStatement.setLong(4, user_id1);
-            if(preparedStatement.executeUpdate() != 0) return SUCCESS_REMOVED;
+            PreparedStatement preparedStatement1 = prepareStatement("delete from friendships where first_user_id = ? and second_user_id = ?", user_id1, user_id2);
+            PreparedStatement preparedStatement2 = prepareStatement("delete from friendships where (first_user_id = ? and second_user_id = ?)", user_id2, user_id1);
+
+            if(preparedStatement1.executeUpdate() != 0 && preparedStatement2.executeUpdate() != 0) return SUCCESS_REMOVED;
+
         } catch (SQLException e) {
             return FAILED_REMOVED;
         }
@@ -112,20 +111,14 @@ public class FriendsSQL implements FriendsDao {
     @Override
     public boolean checkIfFriends(Long user_id1, Long user_id2) {
         try {
-            // Establish a database connection
-            Connection connection = dataSource.getConnection();
 
-            // Prepare the SQL query to check if a friendship exists
-            String query = "select * from friendships where (first_user_id = ? and second_user_id = ?) or (first_user_id = ? and second_user_id = ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setLong(1, user_id1);
-            preparedStatement.setLong(2, user_id2);
-            preparedStatement.setLong(3, user_id2);
-            preparedStatement.setLong(4, user_id1);
+            PreparedStatement preparedStatement1 = prepareStatement("select * from friendships where first_user_id = ? and second_user_id = ?", user_id1, user_id2);
+            PreparedStatement preparedStatement2 = prepareStatement("select * from friendships where first_user_id = ? and second_user_id = ?", user_id2, user_id1);
 
-            // Execute the query and check if a result is found
-            ResultSet resultSet = preparedStatement.executeQuery();
-            return resultSet.next();
+            ResultSet resultSet1 = preparedStatement1.executeQuery();
+            ResultSet resultSet2 = preparedStatement2.executeQuery();
+
+            return resultSet1.next() && resultSet2.next();
         } catch (SQLException e) {
             return false;
         }
