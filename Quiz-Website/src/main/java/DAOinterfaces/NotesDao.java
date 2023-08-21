7b7f755a7c563343cdf8a36e8171067bc2e1e428
nkeke21 <nkeke21@freeuni.edu.ge>
2023-08-21 15:08:14 +0400

    Added comments in Interface: NotesDao

--- Quiz-Website/src/main/java/DAOinterfaces/NotesDao.java
+++ Quiz-Website/src/main/java/DAOinterfaces/NotesDao.java
@@ -4,13 +4,21 @@ import Objects.Note;
 
 import java.util.List;
 
+// An interface representing the contract for interacting with notes in a data storage.
 public interface NotesDao {
+
+    // Constants for result status of note operations
     int SUCCESS_SENT = 1;
     int FAILED_SENT = 2;
     int SUCCESS_DELETED = 3;
     int FAILED_DELETED = 4;
 
+    // Sends a note from a sender to a receiver.
     int send_note(Long sender_id, Long receiver_id, String text);
+
+    // Deletes a note sent from a sender to a receiver.
     int delete_note(Long sender_id, Long receiver_id, String text);
+
+    // Retrieves a list of notes associated with a user.
     List<Note> get_notes(Long user_id);
 }
