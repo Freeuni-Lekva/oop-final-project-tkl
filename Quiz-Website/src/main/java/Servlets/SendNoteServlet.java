3b142415b834cdcb5aece1da35fa75f5b42c24d9
nkeke21 <nkeke21@freeuni.edu.ge>
2023-08-21 17:33:32 +0400

    Added comments for SendNoteServlet class

--- Quiz-Website/src/main/java/Servlets/SendNoteServlet.java
+++ Quiz-Website/src/main/java/Servlets/SendNoteServlet.java
@@ -21,18 +21,24 @@ public class SendNoteServlet extends HttpServlet {
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Retrieve the NotesDao instance from the servlet context using its attribute name.
         NotesDao notesDao = (NotesDao) request.getServletContext().getAttribute(NotesDao.ATTRIBUTE_NAME);
 
+        // Retrieve the main user's ID from the session and convert it to a Long.
         String main_user_id = (String) request.getSession().getAttribute("MainUserID");
         Long mainId = Long.parseLong(main_user_id);
 
+        // Retrieve the friend's ID from the request parameter and convert it to a Long.
         String frId = request.getParameter("friend_id");
         Long friendId = Long.parseLong(frId);
 
+        // Retrieve the note text from the request parameter.
         String text = request.getParameter("noteText");
 
+        // Call the sendNote method of the NotesDao to send a note from the main user to the friend.
         notesDao.sendNote(mainId, friendId, text);
 
+        // Forward the request and response to the userFriends.jsp page to continue processing.
         RequestDispatcher rd = request.getRequestDispatcher("userFriends.jsp");
         rd.forward(request, response);
     }
